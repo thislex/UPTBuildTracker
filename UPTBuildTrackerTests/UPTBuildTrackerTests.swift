19f6380aab37bd5600d38a65a6e10dd09ae808ab
Lexter T <lexter.tapawan@gmail.com>
2025-10-07 21:21:04 -0700

    Initial Commit

---
+++ UPTBuildTrackerTests/UPTBuildTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UPTBuildTrackerTests.swift
+//  UPTBuildTrackerTests
+//
+//  Created by Lexter Tapawan on 10/7/25.
+//
+
+import Testing
+@testable import UPTBuildTracker
+
+struct UPTBuildTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
